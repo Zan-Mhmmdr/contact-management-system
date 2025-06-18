78dd4fe8aee3141276b6cdfffaaa7cdb05fb49db
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-06-19 02:53:38 +0700

    first commit

---
+++ frontend-cms/src/lib/api/UserApi.ts
@@ -0,0 +1,12 @@
+const userRegister = async (user: { username: string, name: string, password: string }) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}`, {
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(user),
+    })
+
+}
+
+export default userRegister;
---
+++ frontend-cms/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './components/Layout'
+import UserRegister from './components/User/UserRegister'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/register' element={<UserRegister />} />
+        </Route>
+        <Route path='dashboard'>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
