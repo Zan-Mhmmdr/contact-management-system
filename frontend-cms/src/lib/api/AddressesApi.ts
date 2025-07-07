1581768e57896dea5d08871869e7e26bb740aaf9
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-07-07 18:27:56 +0700

    feat: add contact list addresses

--- frontend-cms/src/lib/api/AddressesApi.ts
+++ frontend-cms/src/lib/api/AddressesApi.ts
@@ -19,4 +19,17 @@ export const addressesCreate = async (token: any, id: any, Address: Address) => 
         body: JSON.stringify({
             Address
         })
-)
+    })
+}
+
+
+export const addressesList = async (token: any, id: any) => {
+    return fetch(`${import.meta.env.VITE_API_URL}/contacts/${id}/addresses`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': token,
+        }
+    })
+}
